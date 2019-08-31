6652bbe030e862cfe8f33985b8aab9d61a0607cb
Sanjana Sunil <sanjana99sunil@gmail.com>
2019-08-31 22:41:59 +0530

    added tokenising based on space and comma

--- src/main.cpp
+++ src/main.cpp
@@ -6,11 +6,10 @@ int main(int argc, char *argv[]) {
     if(argc != 2)
     {
         std::cout << "Usage: ./a.out \"SQL query\" \n";
+        return 0;
     }
-    else
-    {
-        parse_query(argv[1]);
-    }
+
+    parse_query(argv[1]);
 
     return 0;
 }
--- src/parser.cpp
+++ src/parser.cpp
@@ -1,7 +1,20 @@
 #include <iostream>
+#include <vector>
+#include <cstring>
+#include <string>
 
 void parse_query(char* query) {
 
-    std::cout << query << '\n';
+    std::vector<std::string> tokens;
+
+    char *token = std::strtok(query, " ,");
+    while(token != NULL)
+    {
+        tokens.push_back(token);
+        token = std::strtok(NULL, " ,");
+    }
+
+    for(int i=0; i<tokens.size(); ++i) std::cout << tokens[i] << '\n'; 
+
     return;
 } 
